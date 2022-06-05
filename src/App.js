1c6437942f08bbc844a3106fc8dece442b5894f0
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-06-04 23:49:37 -0500

    add html to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Die from './components/Die';
+import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti';
 
 const App = () => {
     return (
-        <main>App</main>
+        <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="instructions">Roll until all dice are the same.
+                Click each die to freeze it at its current value between rolls.</p>
+            <div className="dice-container"></div>
+            <button className="roll-dice">
+                New Game
+            </button>
+        </main>
     );
 };
 
