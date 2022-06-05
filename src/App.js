80c98fb8d8a9d63abbf16e6c7a9045b49b197f81
Jeremy Pena <5196790+jerpena@users.noreply.github.com>
2022-06-05 01:52:52 -0500

    add newgame functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Die from './components/Die';
 import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
@@ -43,6 +43,16 @@ const App = () => {
         })
     );
 
+    useEffect(() => {
+        const allHeld = dice.every(die => die.isHeld);
+        const firstValue = dice[0].value;
+        const allSameValue = dice.every(die => die.value === firstValue);
+        if (allHeld && allSameValue) {
+            setTenzies(true);
+            console.log("You won!");
+        }
+    }, [dice]);
+
     const diceElements = dice.map(die => (
         <Die
             key={die.id}
@@ -54,12 +64,18 @@ const App = () => {
 
     return (
         <main>
+            {tenzies && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same.
                 Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container"></div>
-            <button className="roll-dice">
-                New Game
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            <button
+                className="roll-dice"
+                onClick={rollDice}
+            >
+                {tenzies ? "New Game" : "Roll"}
             </button>
         </main>
     );
